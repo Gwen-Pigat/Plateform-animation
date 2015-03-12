74faafb164263882bb6f6d490aecfa82099a840b
Gwen-Pigat <pixofheaven@gmail.com>
2015-03-12 17:22:00 +0100

    Ajouts nombreuses modifs visuelles

--- src/Plateforme/AnimationTerritoireBundle/Resources/public/js/chat.js
+++ src/Plateforme/AnimationTerritoireBundle/Resources/public/js/chat.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $(".chatIMG").click(function(){
+    $(".chat-open").click(function(){
         $(".chat").toggle(1000);
+        $(".map-open").toggle(1000);
     });
+    $(".map-open").click(function(){
+        $("iframe").toggle(1000);
+        $(".chat-open").toggle(1000);
+    });
+
+
 });
