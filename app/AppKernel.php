00067ee337c96f7594dc635f8add6009efb955b4
Gwen-Pigat <pixofheaven@gmail.com>
2015-03-25 11:02:12 +0100

    Modifs interface login + MAJ barre nav + Routing + Sonata

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,9 +22,10 @@ class AppKernel extends Kernel
             new Sonata\CoreBundle\SonataCoreBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-
-            // Then add SonataAdminBundle
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
